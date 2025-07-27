bec22a0088e3c3009f459e063a6567d28d9c7c3d
RAJAT <rajat1234chauhan@gmail.com>
2025-07-27 09:14:10 +0530

    fix 23

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // This is the crucial change.
+    // It prevents Vite from deleting the compiled server code.
+    emptyOutDir: false,
+  },
   server: {
     proxy: {
       // This proxies any request starting with /api to your backend server
-      '/api': {
-        target: 'http://localhost:3000',
+      "/api": {
+        target: "http://localhost:3000",
         changeOrigin: true,
         secure: false,
       },
