724cc9cac63c3a3531a42077402a03427c499741
RAJAT <rajat1234chauhan@gmail.com>
2025-07-27 09:36:30 +0530

    fix 29

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-const { defineConfig } = require('vite');
-const react = require('@vitejs/plugin-react-swc');
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-module.exports = defineConfig({
+export default defineConfig({
   plugins: [react()],
   build: {
     // This prevents Vite from deleting the compiled server code.
